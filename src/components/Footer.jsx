bcc24503501aee816d817b852508800594ff8a5c
Legendxkilla <89199553+Legendxkilla@users.noreply.github.com>
2025-08-03 18:02:20 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800">
       <div className="container mx-auto px-4 py-4 text-center text-gray-500 text-sm">
-        Crafted by Burner J
+        Crafted by legendxkilla.eth
       </div>
     </footer>
   );
